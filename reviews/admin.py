6a94ba5eeac6d6e8d5ea2775ba0cf6efb2fced3c
Godllolzzz <sudarshanrajpit2002@gamil.com>
2023-08-29 23:34:03 +0530

    modelForm

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "review_text", "rating")
+    
+admin.site.register(Review, ReviewAdmin)
--- reviews/forms.py
+++ reviews/forms.py
@@ -1,5 +1,28 @@
 from django import forms
+from .models import Review
 
-class ReviewForm(forms.Form):
-    user_name = forms.CharField()
-    
+
+# class ReviewForm(forms.Form):
+#     user_name = forms.CharField(label="Your Name", max_length=100, error_messages={
+#         "required": "Your name  must not be empty!",
+#         "max_length": "Please write a shorter name!"
+#     })
+#     review_text = forms.CharField(label="Your Feedback", max_length=200, widget=forms.Textarea)
+#     rating = forms.IntegerField(label="Your Rating", max_value=5, min_value=1)
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = '__all__'
+        labels = {
+            "user_name": "Your Name",
+            "review_text": "Your Feedback",
+            "rating": "Your Rating"
+        }
+        error_messages = {
+            "user_name": {
+                "required": "Your name  must not be empty!",
+                "max_length": "Please write a shorter name!"
+            }
+        }
+        # we can also use exclude for excluding a list of field of model
